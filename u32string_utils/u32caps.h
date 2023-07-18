0a36837bf3ba148111a054d53c6605d913a88612
Simeon Petrov <simeon.petrov@egt-bg.com>
2023-07-18 10:35:13 +0300

    [u32caps] add tolower and toupper without using std::locale.

---
+++ u32string_utils/u32caps.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace u32
+{
+    char32_t toupper(char32_t ch);
+
+    char32_t tolower(char32_t ch);
+
+    bool toupper_case(char32_t& input);
+
+    bool tolower_case(char32_t& input);
+}
