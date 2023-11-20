74d68e6e8103a0f2c288416a335b6b4259e1eec5
Simeon Petrov <simeon.petrov@egt-bg.com>
2023-11-20 13:55:42 +0200

    [u32caps] add functions for is_upper, is_lower, is_alpha, is_digit, is_space, is_punctuation, is_currency_symbol.

--- u32string_utils/u32caps.h
+++ u32string_utils/u32caps.h
@@ -9,4 +9,20 @@ namespace u32
     bool toupper_case(char32_t& input);
 
     bool tolower_case(char32_t& input);
+
+    bool is_upper(char32_t ch);
+
+    bool is_lower(char32_t ch);
+
+    bool is_alpha(char32_t ch);
+
+    bool is_digit(char32_t ch);
+
+    bool is_space(char32_t ch);
+
+    bool is_punctuation(char32_t ch);
+
+    bool is_other_symbol(char32_t ch);
+
+    bool is_currency_symbol(char32_t ch);
 }
